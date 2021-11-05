2c7edb6b47b50bf299ebe67f42a29ab92d81b258
Nadia-ready <nadia.epivent@gmail.com>
2021-11-05 14:45:07 +0100

    modification entity user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,13 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Entity(repositoryClass=App\Repository\UserRepository::class)
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
